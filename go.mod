c655759b661419142c1ed3be40c106736413a3a9
Yamasaki pan961 <atusi0820522@gmail.com>
2019-12-06 02:41:17 +0900

    HTML表示できたよ

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Yamasaki-pan961/StaticHTML
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.5.0 // indirect
--- server.go
+++ server.go
@@ -2,8 +2,17 @@ package main
 
 import(
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main(){
-	fmt.Printf("Hello World!!")
+	fmt.Printf("サーバー動きます")
+	router:= gin.Default()
+	router.LoadHTMLGlob("*.html")
+
+	router.GET("/",func(c *gin.Context){
+		c.HTML(http.StatusOK,"index.html", gin.H{})
+	})
+	router.Run(":8080")
 }
